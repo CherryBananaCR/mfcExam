e4260a8ad1ec3a4e78e2576fd1117efe507cee8a
CherryBananaCR <138956832+CherryBananaCR@users.noreply.github.com>
2023-12-22 17:18:24 +0900

    insert DlgImage
    이미지 다이얼로그를 삽입함

---
+++ gSolution/gPrj/DlgImage.cpp
@@ -0,0 +1,44 @@
+﻿// DlgImage.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "gPrj.h"
+#include "afxdialogex.h"
+#include "DlgImage.h"
+
+
+// DlgImage 대화 상자
+
+IMPLEMENT_DYNAMIC(DlgImage, CDialogEx)
+
+DlgImage::DlgImage(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DlgImage, pParent)
+{
+	m_pParent = pParent;
+}
+
+DlgImage::~DlgImage()
+{
+}
+
+void DlgImage::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(DlgImage, CDialogEx)
+	ON_BN_CLICKED(IDC_BTN_UP_PARENT, &DlgImage::OnBnClickedBtnUpParent)
+END_MESSAGE_MAP()
+
+
+// DlgImage 메시지 처리기
+
+#include "gPrjDlg.h"
+void DlgImage::OnBnClickedBtnUpParent()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	static int n = 100;
+	CgPrjDlg* pDlg = (CgPrjDlg*)m_pParent;
+	pDlg->callFunc(n++);
+}
---
+++ gSolution/gPrj/DlgImage.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// DlgImage 대화 상자
+
+class DlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(DlgImage)
+
+public:
+	DlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~DlgImage();
+
+
+	CWnd* m_pParent;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DlgImage };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnUpParent();
+};
