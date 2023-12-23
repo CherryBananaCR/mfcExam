b3f2b2e81372a468d68397d0fed5132bc4612a6f
CherryBananaCR <138956832+CherryBananaCR@users.noreply.github.com>
2023-12-23 19:24:13 +0900

    우측 이미지에 정보 전달

--- gSolution/gPrj/DlgImage.h
+++ gSolution/gPrj/DlgImage.h
@@ -12,7 +12,7 @@ public:
 	DlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
 	virtual ~DlgImage();
 
-
+	CImage m_image;
 	CWnd* m_pParent;
 
 // 대화 상자 데이터입니다.
@@ -26,4 +26,12 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedBtnUpParent();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	int m_nDataCount = 0;
+	CPoint m_ptData[100];
+
+private:
+	void InitImage();
+	void drawData(CDC* pDC);
 };
